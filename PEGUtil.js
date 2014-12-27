33c1193ed31f0b6b242b9fa22f47e6b18b8b3808
Ralf S. Engelschall <rse@engelschall.com>
2014-12-27 08:57:00 +0100

    use double-quotes

--- PEGUtil.js
+++ PEGUtil.js
@@ -304,10 +304,10 @@
                 .replace(/\n/g,   "\\n")
                 .replace(/\f/g,   "\\f")
                 .replace(/\r/g,   "\\r")
-                .replace(/[\x00-\x07\x0B\x0E\x0F]/g, function(ch) { return '\\x0' + hex(ch); })
-                .replace(/[\x10-\x1F\x80-\xFF]/g,    function(ch) { return '\\x'  + hex(ch); })
-                .replace(/[\u0100-\u0FFF]/g,         function(ch) { return '\\u0' + hex(ch); })
-                .replace(/[\u1000-\uFFFF]/g,         function(ch) { return '\\u'  + hex(ch); });
+                .replace(/[\x00-\x07\x0B\x0E\x0F]/g, function(ch) { return "\\x0" + hex(ch); })
+                .replace(/[\x10-\x1F\x80-\xFF]/g,    function(ch) { return "\\x"  + hex(ch); })
+                .replace(/[\u0100-\u0FFF]/g,         function(ch) { return "\\u0" + hex(ch); })
+                .replace(/[\u1000-\uFFFF]/g,         function(ch) { return "\\u"  + hex(ch); });
         };
         return {
             prolog: extract(txt, b, o - b),
