197cd9843ae2a89ea13a20477090918005a69ed8
Ralf S. Engelschall <rse@engelschall.com>
2015-01-25 19:20:00 +0100

    update dependencies and upgrade examples to ASTy 1.0.0

--- sample.js
+++ sample.js
@@ -3,11 +3,12 @@ var ASTY    = require("asty")
 var PEG     = require("pegjs")
 var PEGUtil = require("./PEGUtil")
 
+var asty = new ASTY()
 var parser = PEG.buildParser(fs.readFileSync("sample.pegjs", "utf8"))
 var result = PEGUtil.parse(parser, fs.readFileSync(process.argv[2], "utf8"), {
     startRule: "start",
     makeAST: function (line, column, offset, args) {
-        return ASTY.apply(null, args).pos(line, column, offset)
+        return asty.create.apply(asty, args).pos(line, column, offset)
     }
 })
 if (result.error !== null)
